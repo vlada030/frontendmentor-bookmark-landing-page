f073fe5d68798ba1c0887dc4f1231dd69cad47fe
Vlada DELL <vlada030@gmail.com>
2021-10-13 21:34:47 +0200

    mobile navigation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './scss/main.scss'
+
+const closeMobMenu = document.getElementById('close-mob-nav')
+const openMobMenu = document.getElementById('open-mob-nav')
+const mobileMenu = document.getElementById('mobileMenu')
+
+openMobMenu.addEventListener('click', () => {
+        mobileMenu.classList.remove('disabled')
+        document.getElementsByClassName('top-nav').classList.add('disabled')
+    })
+
+// [openMobMenu, closeMobMenu].forEach(button => {
+//     console.log(button);
+//     button.addEventListener('click', () => {
+//         mobileMenu.classList.toggle('disabled')
+//         document.getElementsByClassName('top-nav').classList.toggle('disabled')
+//     })
+// }); 
