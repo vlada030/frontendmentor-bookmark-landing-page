360cc63dc355b1aff6cf1b7dc26f7be069e1b493
Vlada FKZ Laptop <vlada030@gmail.com>
2021-10-14 09:46:48 +0200

    postavljena navigacija

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import './scss/main.scss'
 
-const closeMobMenu = document.getElementById('close-mob-nav')
 const openMobMenu = document.getElementById('open-mob-nav')
+const closeMobMenu = document.getElementById('close-mob-nav')
 const mobileMenu = document.getElementById('mobileMenu')
 
-openMobMenu.addEventListener('click', () => {
-        mobileMenu.classList.remove('disabled')
-        document.getElementsByClassName('top-nav').classList.add('disabled')
-    })
+let buttons = [closeMobMenu, openMobMenu]
 
-// [openMobMenu, closeMobMenu].forEach(button => {
-//     console.log(button);
-//     button.addEventListener('click', () => {
-//         mobileMenu.classList.toggle('disabled')
-//         document.getElementsByClassName('top-nav').classList.toggle('disabled')
-//     })
-// }); 
+buttons.forEach(button => {
+    console.log(button);
+    button.addEventListener('click', () => {
+        mobileMenu.classList.toggle('animate')
+    
+    })
+}); 
