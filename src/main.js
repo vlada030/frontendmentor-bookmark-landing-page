3a85047981265759b174739a0b014a9dbfb756dd
Vlada FKZ Laptop <vlada030@gmail.com>
2021-10-15 14:53:35 +0200

    OBAVEZNO Z INDEX NA PARENT!!!

--- src/main.js
+++ src/main.js
@@ -4,10 +4,32 @@ const openMobMenu = document.getElementById('open-mob-nav')
 const closeMobMenu = document.getElementById('close-mob-nav')
 const mobileMenu = document.getElementById('mobileMenu')
 
+const simpleBtn = document.getElementById('simpleBtn')
+const speedyBtn = document.getElementById('speedyBtn')
+const easyBtn = document.getElementById('easyBtn')
+
+let featureButtons = [simpleBtn, speedyBtn, easyBtn]
+
+featureButtons.forEach(button => {
+    button.addEventListener('click', e => {
+
+        featureButtons.forEach(btn => {
+            btn.parentNode.classList.toggle('active', false)
+        })
+
+        document.querySelectorAll('.card').forEach(elem => {
+            console.log(elem);
+            elem.classList.toggle('disabled', true)
+        })
+
+        // e.target
+    })
+})
+
 let buttons = [closeMobMenu, openMobMenu]
 
 buttons.forEach(button => {
-    console.log(button);
+    //console.log(button);
     button.addEventListener('click', () => {
         mobileMenu.classList.toggle('animate')
     
